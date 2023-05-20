6f3470afcbdc48a8d3adf4cc0e825bf6ad84e29c
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-05-20 22:05:03 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-bucket"
+    bucket         = "pocbucket1185"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     encrypt        = true
-    dynamodb_table = "terraform_locks"
+    dynamodb_table = "terraform_backend"
   }
 }

bd908a03d1e5e56e0c5b91e8c661c58f4be40608
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-05-20 22:18:16 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -3,5 +3,8 @@ terraform {
     bucket         = "pocbucket1185"
     key            = "terraform.tfstate"
     region         = "us-east-2"
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
   }
 }
